85b5a31c3aced6203c44530ed14da02753d8cb06
Nate Nadeau <nate@inspiredbyspire.com>
2014-11-01 16:10:51 -0400

    Initial 48-hour submission

--- README.md
+++ README.md
@@ -4,7 +4,7 @@ simple-google-calendar
 A simple client-side HTML/javascript app to access Google calendar events for a given date, and adding new events. It requires 
 a Google authentication by the user (via OAuth2), and then will show events for the selected date, defaulting to the current date.
 
-The user may also add a new event to (@TODO figure out which of the user's calendars to write to).
+The user may also add a new event (to simplify things I default to using the user's primary calendar, and I use the quickAdd api call).
 
 I've divided up the business logic JS into a few different files, which makes maintenance/development/project-layout more clear, but
 comes at the cost of extra HTTP requests to load the several files. In production you could have a preprocessing step of some sort
@@ -13,3 +13,16 @@ for apps in production mode).
 
 __NOTE__: For this challenge I had to learn Google APIs (Calendar specifically), Google OAuth2, Backbone.js, and SASS all mostly from
 scratch as I have never used any of these so far in my professional projects. So yes, it was a *challenge* for sure :)
+
+__NOTE__: I defined the project on the Google Developers Console to expect to be running from http://localhost (Javascript Origins setting),
+so you may need to rig up your checkout to run from that identical host or you will get authorization failures from Google.
+
+__NOTE__: I developed/tested with latest Chrome. However <input type="date"> is not as nicely supported in browsers such as IE and
+Firefox, so with more time I would need to add more sanitization of the date input value for those browsers, but I didn't get to it.
+
+__NOTE__: There's still some functional massaging I could do for full day events if I had more time. Also I wasn't really sure which
+values to display for each calendar event, so I just show start, end, and title (summary). Other fields from the API could easily be added.
+
+__NOTE__: The Add New Event as I have coded it does not check the date field, it just uses whatever Quick Add text you type in to
+determine the datetime of the event. With more time I could envision developing a more fully functional form for defining the new event,
+and using the insert API call instead of quickAdd.
--- js/sgc.js
+++ js/sgc.js
@@ -4,15 +4,16 @@ $(function(){
   // the event model
   var CalEvent = Backbone.Model.extend({
     defaults: function(){
+      var now = new Date();
       return {
-        start: new Date(),
-        duration: 60,
+        start: now,
+        end: new Date(now.getTime() + (3600 * 1000)), // 1 hour later
         title: 'Default Event',
-        all_day: false
         // add any more values we want to model from the google calendar event object here
       }
     }
   });
+  
   
   // the list of event instances
   var CalEventList = Backbone.Collection.extend({
@@ -20,6 +21,7 @@ $(function(){
     comparator: 'start'
   });
   var CalEvents = new CalEventList;
+  
   
   // view for an event
   var CalEventView = Backbone.View.extend({
@@ -41,20 +43,40 @@ $(function(){
     }
   });
   
-  // the application view @TODO
+  
+  // the application view
   var AppView = Backbone.View.extend({
     el: $('#sgc-app'),
     
     events: {
-      
+      'click #cal-go': 'refreshDate',
+      'click #cal-new': 'addNewEvent'
     },
     
     initialize: function(){
       this.input = this.$('#cal-date');
+      this.input.val(new Date().toDateInputValue());
       this.container = this.$('#event-container');
+      this.authbutton = this.$('#authorize');
+      this.neweventform = this.$('#new-event-container');
+      this.newbutton = this.$('#cal-new');
+      this.neweventdesc = this.$('#cal-new-desc');
       
+      // simple array of calevents that will then be assigned in batch to CalEvents via CalEvents.reset()
+      this.calevents = [];
+      
+      // Users may add/remove calendars from their Google calendar list while interacting with this app, but to keep things
+      // simpler and  faster, we'll cache the calendarlist the first time, and just use that; otherwise I think we'd have to 
+      // get the calendar list every time we want to get a set of events for a date.
+      this.calendarlist = null;
+      
+      // listen for reset event on our CalEventList instance
       this.listenTo(CalEvents, 'reset', this.render);
-      // @TODO maybe put the google auth stuff here?
+      
+      if (gapiIsLoaded)
+        this.checkAuth(this.authInfo.scopeReadOnly, true);
+      else
+        this.listenForApi();
     },
     
     render: function(){
@@ -65,14 +87,177 @@ $(function(){
     addEvent: function(calevent){
       var view = new CalEventView({model: calevent});
       this.container.append(view.render().el);
+    },
+    
+    listenForApi: function(){
+      // @NOTE I'm aware that I'm using two different ways of handling the value of 'this'... here I use another variable, and 
+      // further below I use $.proxy()...normally I would consistently do one way in code, but for illustration purposes I'm doing both
+      var theApp = this;
+      $(document).on(GAPI_LOADED_EV, function(e){
+        theApp.checkAuth(theApp.authInfo.scopeReadOnly, true);
+      })
+    },
+    
+    // @NOTE I defined the project on the Google Developers Console to expect to be running from http://localhost (Javascript Origins setting)
+    authInfo: {
+      clientID: '205919807575-2p3tlh5i5pi93lm8r96ufq6jup14179v.apps.googleusercontent.com',
+      scopeReadOnly: 'https://www.googleapis.com/auth/calendar.readonly',
+      scopeReadWrite: 'https://www.googleapis.com/auth/calendar'
+    },
+    
+    checkAuth: function(scope, immediate){
+      gapi.auth.authorize({client_id: this.authInfo.clientID, scope: scope, immediate: immediate}, $.proxy(this.handleAuthResult, this));
+    },
+    
+    handleAuthResult: function(authResult){
+      // hide the auth button and show the new event form on success, otherwise vice versa
+      if (authResult && !authResult.error){
+        this.authbutton.addClass('hidden');
+        this.neweventform.removeClass('hidden');
+        this.apiEventsForDate(this.input.val());
+      } else {
+        this.authbutton.removeClass('hidden');
+        this.neweventform.addClass('hidden');
+        this.authbutton.one('click', $.proxy(this.handleAuthClick, this));
+        // for this simple app, just put message and info to console on auth error...
+        console.log('auth error!');
+        console.log(authResult);
+      }
+    },
+    
+    handleAuthClick: function(e){
+      this.checkAuth(this.authInfo.scopeReadOnly, false);
+    },
+    
+    apiEventsForDate: function(date){
+      var theApp = this;
+      gapi.client.load('calendar', 'v3', function(){
+        // initialize the calendarlist if needed
+        if (!theApp.calendarlist){
+          var request = gapi.client.calendar.calendarList.list();
+          request.then(function(resp){
+            theApp.setCalendarList(resp.result);
+            theApp.apiGetEvents(date);
+          }, function(resp){
+            console.log('Error getting calendar list!');
+            console.log(resp);
+          });
+        } else {
+          theApp.apiGetEvents(date);
+        }
+      });
+    },
+    
+    apiGetEvents: function(date){
+      var theApp = this;
+      theApp.calevents = [];
+      
+      // to track the several async requests
+      var reqTotal = theApp.calendarlist.length;
+      var reqCount = 0;
+      
+      // for each google calendar, get events for the specified date, then for each event found, build a CalEvent and add it to
+      // theApp.calevents for each, then reset the CalEvents list with theApp.calevents.
+      theApp.calendarlist.forEach(function(cal){
+        // handle timezone offset
+        var localDate = new Date(date);
+        var datetime = new Date(localDate.getTime() + (localDate.getTimezoneOffset() * 60 * 1000));
+        var request = gapi.client.calendar.events.list({
+          calendarId: cal,
+          timeMin: datetime.toISOString(),
+          timeMax: new Date(datetime.getTime() + (24 * 3600 * 1000)).toISOString()
+        });
+        request.then(function(resp){
+          resp.result.items.forEach(function(calevent){
+            var theStart = calevent.start.dateTime || calevent.start.date;
+            var theEnd = calevent.end.dateTime || calevent.end.date; 
+            theApp.calevents.push(new CalEvent({
+              title: calevent.summary,
+              start: new Date(theStart),
+              end: new Date(theEnd)
+            }));
+          });
+          reqCount++;
+          
+          // if we now have requests for all calendars finished, then finally batch update the CalEvents list
+          if (reqCount == reqTotal){
+            CalEvents.reset(theApp.calevents);
+          }
+        }, function(resp){
+          console.log('Error retrieving events for date: ' + date + ' and calendar: ' + cal);
+          console.log(resp);
+          reqCount++;
+        });
+      });
+    },
+    
+    setCalendarList: function(calendarlist){
+      this.calendarlist = [];
+      calendarlist.items.forEach($.proxy(function(cal){
+        // just hold the IDs of the calendars for futher API calls
+        this.calendarlist.push(cal.id);
+      }), this);
+    },
+    
+    refreshDate: function(e){
+      this.checkAuth(this.authInfo.scopeReadOnly, true);
+      return false;
+    },
+    
+    addNewEvent: function(e){
+      if (!this.calendarlist || this.calendarlist.length == 0){
+        alert('You have no valid calendars to add to!');
+        return false;
+      }
+      
+      if (this.neweventdesc.val() == ''){
+        alert('You have not specified a description!');
+        return false;
+      }
+      
+      // check for write authorization then do the event creation if we succeed
+      var theApp = this;
+      gapi.auth.authorize({
+        client_id: this.authInfo.clientID,
+        scope: this.authInfo.scopeReadWrite, immediate: false
+      }, function(authResult){
+        if (authResult && !authResult.error){
+          console.log('HERE?')
+          // @NOTE just default to the first calendar which I think Google always returns your primary calendar first when retrieving the
+          // calendar list, but I did not fully verify that....
+          var theCalendar = theApp.calendarlist[0];
+          console.log(gapi.client.calendar.events);
+          console.log(theApp.neweventdesc.val());
+          var request = gapi.client.calendar.events.quickAdd({
+            calendarId: theCalendar,
+            text: theApp.neweventdesc.val()
+          });
+          request.then(function(resp){
+            console.log('success?');
+            theApp.neweventdesc.val('');
+            theApp.refreshDate();
+          }, function(resp){
+            console.log('Error creating new event!');
+            console.log(resp);
+          });
+
+        } else {
+          // for this simple app, just put message and info to console on auth error...
+          console.log('auth write error!');
+          console.log(authResult);
+          alert('Failed to gain write authorization to your primary google calendar!')
+        }
+      });
+      
+      return false;      
     }
   });
   
   var App = new AppView();
   
   /// @TODO TESTING
-  var ev1 = new CalEvent();
-  var ev2 = new CalEvent({start: new Date('2014-11-02 00:00:00'), duration: 90, title: 'Future Event'});
-  var ev3 = new CalEvent({start: new Date(), duration: 90, title: 'Future Woohoo Event'});
-  CalEvents.reset([ev1, ev2, ev3]);
+  /*var ev1 = new CalEvent();
+  var ev2 = new CalEvent({start: new Date('2014-11-02 00:00:00'), end: new Date('2014-11-02 01:00:00'), title: 'Future Event'});
+  var ev3 = new CalEvent({start: new Date(), end: new Date(new Date().getTime() + (3600 * 1000)), title: 'Future Woohoo Event'});
+  CalEvents.reset([ev1, ev2, ev3]);*/
 });
